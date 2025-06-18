21aca40e24ae8838ef895b14688320b9f23e2574
Dawitmeh <dawitm96@gmail.com>
2025-06-18 16:37:12 +0300

    latest_updates

--- app/Http/Controllers/Api/DocumentController.php
+++ app/Http/Controllers/Api/DocumentController.php
@@ -76,10 +76,21 @@ class DocumentController extends Controller
             }
 
             $expectedMime = [
-                'pdf' => 'application/pdf',
-                'jpg' => 'image/jpeg',
-                'jpeg' => 'image/jpeg',
-                'png' => 'image/png',
+                 'pdf' => 'application/pdf',
+                'doc' => 'application/msword',
+                'docx' => 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+                'xls' => 'application/vnd.ms-excel',
+                'xlsx' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                'ppt' => 'application/vnd.ms-powerpoint',
+                'pptx' => 'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+                'txt' => 'text/plain',
+                'rtf' => 'application/rtf',
+                'odt' => 'application/vnd.oasis.opendocument.text',
+                'ods' => 'application/vnd.oasis.opendocument.spreadsheet',
+                'odp' => 'application/vnd.oasis.opendocument.presentation',
+                'csv' => 'text/csv',
+                'html' => 'text/html',
+                'htm' => 'text/html',
             ][$documentType->format] ?? null;
 
             if (!$expectedMime || $mimeType !== $expectedMime) {
